ef88cfdbd70ebb17fa795b4a92da6543f78a644b
alexmonteil <alex.monteil@outlook.com>
2022-09-21 13:29:26 -0500

    updated subsequence

--- subsequence.py
+++ subsequence.py
@@ -4,23 +4,21 @@
 
 def is_subsequence(str1, str2):
     
-    i1, i2 = 0, 0
-    j1, j2 = len(str1) - 1, len(str2) - 1
+    str1len, str2len = len(str1), len(str2)
+    i, j = 0, 0
     
-    
-    while i2 <= j2:
-        if str2[i2] == str1[0]:
-            break
-        else:
-            i2 += 1
+    while j < str2len:
+        if str1[i] == str2[j]:
+            i += 1
             
-    while i1 <= j1:
-        if str1[i1] != str2[i2]:
-            return False
-        i1 += 1
-        i2 += 1
+        if i == str1len:
+            return True
         
-    return True
+        j += 1
+            
+    return False
+    
+                        
 
 
 teststr1, teststr2 = "hello", "hello world"
@@ -29,6 +27,6 @@ teststr5, teststr6 = "abc", "abracadabra"
 teststr7, teststr8 = "abc", "acb"
 
 print(f"Should print True: {str(is_subsequence(teststr1, teststr2))}")
-print(f"Should print False: {str(is_subsequence(teststr3, teststr4))}")
-print(f"Should print False: {str(is_subsequence(teststr5, teststr6))}")
+print(f"Should print True: {str(is_subsequence(teststr3, teststr4))}")
+print(f"Should print True: {str(is_subsequence(teststr5, teststr6))}")
 print(f"Should print False: {str(is_subsequence(teststr7, teststr8))}")
