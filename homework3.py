3b1326179b8cf71219a14d927bbc166aebd84da9
alexmonteil <alex.monteil@outlook.com>
2022-11-22 23:17:27 -0600

    added homework3

---
+++ homework3.py
@@ -0,0 +1,29 @@
+def rooty_print(matrix):
+    print(rooty_printing(matrix, 0, len(matrix) - 1))
+
+def rooty_printing(matrix, i, j):
+    
+    if j < i - 1:
+        return
+    
+    elif j == i - 1:
+        return "d" + str(j)
+    
+    else:
+        r = matrix[i][j]
+        return "k" + str(r) + "(" + rooty_printing(matrix, i, r - 1) + "," + rooty_printing(matrix, r + 1, j) + ")"
+    
+
+matrix = [
+    [1,1,2,2,2],
+    [2,2,2,4],
+    [3,4,5],
+    [4,5],
+    [5]
+]
+
+rooty_print(matrix)
+    
+    
+
+    
