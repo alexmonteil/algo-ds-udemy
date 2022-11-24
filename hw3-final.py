355b717ab2d7aff30eba7d474f1d8144723f2d9c
alexmonteil <alex.monteil@outlook.com>
2022-11-23 22:21:33 -0600

    updated hw3-final

--- hw3-final.py
+++ hw3-final.py
@@ -1,4 +1,5 @@
-# Added additional row to the matrix to simulate Cormen's 1 indexed arrays
+# Added additional row and and column to the matrix to simulate Cormen's 1 indexed arrays and keep the code close to what would work with Cormen's pseudocode.
+# Modified the code on line 4 from: len(matrix) in the pseudocode version to: len(matrix) - 1 to account for row and column modification.
 def rooty_print(matrix):
     print(rooty_printing(matrix, 1, len(matrix) - 1))
 
@@ -22,3 +23,6 @@ matrix = [
 ]
 
 rooty_print(matrix)
+
+# Output:
+# k2(k1(d0,d1),k5(k4(k3(d2,d3),d4),d5))
