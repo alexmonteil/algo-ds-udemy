ea8c3d8fa4be01fadef33e1d1d7fd1ffb9695561
alexmonteil <alex.monteil@outlook.com>
2022-12-10 19:28:49 -0600

    fixed is_brave

--- is_brave_ancestor.py
+++ is_brave_ancestor.py
@@ -2,14 +2,19 @@
 def is_braving(G, u, visited):
     u.mark = True
     visited[u] = u
+    # loop over u's adjacency list
     for v in G.Adj[u]:
-        if v.mark == False and v != u:
-            if not (u in G.Adj[v]) and not (v in visited):
+        # if u's adjacent v is already in visited it means more paths exist mark that v as false as it violates the rule
+        if v in visited:
+            v.mark = False
+         
+        elif v.mark == False:
+            # if u is not in v's adjacency list visit v
+            if not (u in G.Adj[v]):
                 is_braving(G, v)
+            # if u was in v's adjancency list and v was in u's adjancency list, the rule is violated, v must be marked false
             else:
                 visited[v] = v
-        elif v == u:
-            v.mark = False
 
 def is_brave_ancestor(G):
     
@@ -19,8 +24,9 @@ def is_brave_ancestor(G):
     for v in G.V:
         v.mark = False
     
-    # loop over vertices in G, for each vertex if the mark is false and it is not visited call is_braving on it.
+    # loop over vertices in G
     for v in G.V:
+        # if vertex's mark is false and vertex is not in visited then visit vertex
         if v.mark == False and not (v in visited):
             is_braving(G, v, visited)
             
